42314a92e8000b70dc9605fcb0934f8dddc97724
Shoo <root@ishoo.ru>
2017-10-03 14:32:28 +0300

    Initial commit

--- PassGen/Form1.cs
+++ PassGen/Form1.cs
@@ -16,5 +16,32 @@ namespace PassGen
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(generate());
+        }
+
+        public static string generate()
+        {
+            string high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string low = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "1234567890";
+            string password = "";
+
+            Random rnd = new Random();
+            password += high[rnd.Next(0, high.Length)];
+            for (int i = 1; i < 7; i++)
+            {
+                password += low[rnd.Next(0, low.Length)];
+            }
+
+            password += numbers[rnd.Next(0, numbers.Length)];
+
+            return password;
+
+        }
     }
+
+    
 }
