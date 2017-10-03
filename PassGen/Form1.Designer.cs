8931c960917292477680a78c9e66d98e9f72fd48
Shoo <root@ishoo.ru>
2017-10-03 15:07:16 +0300

    v3

--- PassGen/Form1.Designer.cs
+++ PassGen/Form1.Designer.cs
@@ -47,6 +47,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(120, 70);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Passgen";
             this.ResumeLayout(false);
